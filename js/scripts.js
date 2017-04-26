cd77950cee94c1bb4312f1a9f177248230698af5
Michal Slabik <michal.slabik3@wp.pl>
2017-04-26 23:37:50 +0200

    Add js/scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var name = prompt('Wpisz swoje imię');
+//komentarz alert('Witaj, ' + name);
+console.log('Witaj, ' + name);
+// komentarz liniowy
+
+// komentarz wywołania funkcji, aby ją wyłączyć
+// console.log(‘Zakomentowana się nie wywoła’);
+
+/* 
+	Komentarz blokowy
+	Tutaj nic nie będzie działało
+	Koniec komentarza
+*/
+
